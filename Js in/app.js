9029ed14c995aa4292a7864088e3c156cb8b07b6
YaqoobYaqoobi <yaqoobyaqoobi1126@gmail.com>
2026-04-22 22:13:35 +0430

    4/22/2026

--- Js in/app.js
+++ Js in/app.js
@@ -1,17 +1,16 @@
-var x = 10;
- console.log(x);
-
-x= 100;
-console.log(x);
-
-
-
-
-
- const Y = 15;
-
-    console.log(Y);
+// var x = 10;
+//  console.log(x);
+// x= 100;
+// console.log(x);
+//  const Y = 15;
+//     console.log(Y);
+// Y= 20;
+// console.log(Y)
 
 
-Y= 20;
-console.log(Y)
+var Ahamd= 'Hi good buy';
+
+    console.log(Ahamd)
+
+    console.log(Ahamd.length)
+    console.log(Ahamd.type)
